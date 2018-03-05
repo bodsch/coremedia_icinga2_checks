f57cae092c48dfc2abcb57b3f6ccc8c625642437
Bodo Schulz <bodo.schulz@coremedia.com>
2018-03-05 15:16:38 +0100

    add helper class for redis access

---
+++ lib/include/redis.h
@@ -0,0 +1,30 @@
+
+#ifndef REDIS_H
+#define REDIS_H
+
+#include <string>
+#include <cctype>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <map>
+
+// #include <common.h>
+#include "md5.h"
+#include <redox.hpp>
+
+class Redis {
+
+  public:
+                      Redis(const std::string s, int p) { _server = s; _port = p; }
+
+    std::string       cache_key(const std::string hostname, const std::string service);
+    bool              get(const std::string key, std::string &result);
+
+
+  private:
+    std::string _server;
+    int _port;
+};
+
+#endif // REDIS_H
---
+++ lib/src/redis.cpp
@@ -0,0 +1,64 @@
+
+#include "../include/redis.h"
+
+std::string Redis::cache_key(const std::string hostname, const std::string service) {
+
+  /**
+   * calculate the cache key for the data in our redis */
+  std::map<std::string,std::string> d;
+  std::map<std::string,std::string>::iterator it;
+
+  // cacheKey = Storage::RedisClient.cacheKey( { :host => host, :pre => 'result', :service => service } )
+
+  d["service"] = service;
+  d["host"] = hostname;
+  d["pre"] = "result";
+
+  std::string key;
+  std::string key_md5;
+
+  // {:host=>"osmc.local", :pre=>"result", :service=>"caefeeder-live"}
+  key = "{";
+  if( d.size() > 0 ) {
+    for( it = d.begin(); it != d.end(); ++it ) {
+      key.append( ":" + it->first ); // string (key)
+      key.append( "=>" );
+      key.append( "\"" + it->second + "\"" );  // string's value
+
+      if(std::distance(it,d.end())>1) {
+        key.append(", ");
+      }
+    }
+  }
+  key.append("}");
+  key_md5 = md5(key);
+
+  return key_md5;
+}
+
+
+
+bool Redis::get(const std::string key, std::string &result) {
+
+  redox::Redox rdx( std::cerr, redox::log::Off);
+
+  if(!rdx.connect(_server, _port)) {
+    std::cout << "ERROR - Could not connect to Redis: Connection refused." << std::endl;
+    return false;
+  }
+
+  // we use a named database ...
+  rdx.commandSync<std::string>({"SELECT", "1"});
+
+  try {
+    result = rdx.get(key);
+  } catch(...) {
+    std::cout << "WARNING - no data in our data store found."  << std::endl;
+    return false;
+  }
+
+  rdx.disconnect();
+
+  return true;
+}
+
